02aaff59ae4ade4b1b9352f016c6f08f64095226
Sikshya Maharjan <disengagedhero666@gmail.com>
2025-03-06 10:47:33 +0545

    chore: separate file for requests

---
+++ src/api-client/http-client.ts
@@ -0,0 +1,121 @@
+import { AuthUser } from "@/type/auth";
+
+export default class HttpClient {
+  private baseUrl: string;
+
+  constructor(baseUrl: string) {
+    this.baseUrl = baseUrl;
+  }
+
+  private getUserInfo(): AuthUser {
+    const userFromStorage = localStorage.getItem("user") ?? "";
+    if (userFromStorage) {
+      return JSON.parse(userFromStorage);
+    }
+    return {
+      token: "",
+      expiresIn: 0,
+      expired: true,
+    };
+  }
+
+  public async request<T>(
+    endpoint: string,
+    method: string = "GET",
+    headers: Record<string, string> = {},
+    body: any = null
+  ): Promise<T> {
+    // const token = this.getBearerToken();
+
+    const { token } = this.getUserInfo();
+
+    const defaultHeaders: Record<string, string> = {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+      ...headers,
+    };
+
+    const options: RequestInit = {
+      method,
+      headers: defaultHeaders,
+    };
+
+    if (body) {
+      options.body = JSON.stringify(body);
+    }
+
+    const response = await fetch(`${this.baseUrl}${endpoint}`, options);
+
+    if (!response.ok) {
+      const responseJson = await response.json();
+      throw new Error(responseJson.message);
+    }
+    if (method === "DELETE") {
+      return response as T;
+    }
+    return await response.json();
+  }
+
+  public async multiPartRequest<T>(
+    endpoint: string,
+    method: string = "GET",
+    headers: Record<string, string> = {},
+    body: any = null
+  ): Promise<T> {
+    const { token } = this.getUserInfo();
+
+    const defaultHeaders: Record<string, string> = {
+      Authorization: `Bearer ${token}`,
+      ...headers,
+    };
+
+    const options: RequestInit = {
+      method,
+      headers: defaultHeaders,
+      body,
+    };
+
+    const response = await fetch(`${this.baseUrl}${endpoint}`, options);
+
+    if (!response.ok) {
+      const responseJson = await response.json();
+      throw new Error(responseJson.message);
+    }
+    if (method === "DELETE") {
+      return response as T;
+    }
+    return await response.json();
+  }
+
+  public async unauthenticatedRequest<T>(
+    endpoint: string,
+    method: string = "GET",
+    headers: Record<string, string> = {},
+    body: any = null
+  ): Promise<T> {
+    const defaultHeaders: Record<string, string> = {
+      "Content-Type": "application/json",
+      ...headers,
+    };
+
+    const options: RequestInit = {
+      method,
+      headers: defaultHeaders,
+    };
+
+    if (body) {
+      options.body = JSON.stringify(body);
+    }
+
+    const response = await fetch(`${this.baseUrl}${endpoint}`, options);
+
+    if (!response.ok) {
+      const responseJson = await response.json();
+      throw new Error(responseJson.message);
+    }
+    if (method === "DELETE") {
+      return response as T;
+    }
+    return await response.json();
+  }
+}
--- src/api-client/index.ts
+++ src/api-client/index.ts
@@ -4,6 +4,7 @@ import { PaginatedResponse } from "@/type/paginated-response";
 import { Field, Portal } from "@/type/portal";
 import { User, UserPayload } from "@/type/user";
 import { apiUrls } from "./apiUrls";
+import HttpClient from "./http-client";
 
 class ApiClient {
   private baseUrl: string =
@@ -11,103 +12,24 @@ class ApiClient {
 
   // private baseUrl: string = "http://localhost:8080";
 
-  private getBearerToken(): string {
-    return "YOUR_BEARER_TOKEN";
-  }
+  private httpClient: HttpClient;
 
-  private getUserInfo(): AuthUser {
-    const userFromStorage = localStorage.getItem("user") ?? "";
-    if (userFromStorage) {
-      return JSON.parse(userFromStorage);
-    }
-    return {
-      token: "",
-      expiresIn: 0,
-      expired: true,
-    };
-  }
-
-  public async request<T>(
-    endpoint: string,
-    method: string = "GET",
-    headers: Record<string, string> = {},
-    body: any = null
-  ): Promise<T> {
-    // const token = this.getBearerToken();
-
-    const { token } = this.getUserInfo();
-
-    const defaultHeaders: Record<string, string> = {
-      Authorization: `Bearer ${token}`,
-      "Content-Type": "application/json",
-      ...headers,
-    };
-
-    const options: RequestInit = {
-      method,
-      headers: defaultHeaders,
-    };
-
-    if (body) {
-      options.body = JSON.stringify(body);
-    }
-
-    const response = await fetch(`${this.baseUrl}${endpoint}`, options);
-
-    if (!response.ok) {
-      const responseJson = await response.json();
-      throw new Error(responseJson.message);
-    }
-    if (method === "DELETE") {
-      return response as T;
-    }
-    return await response.json();
-  }
-
-  public async unauthenticatedRequest<T>(
-    endpoint: string,
-    method: string = "GET",
-    headers: Record<string, string> = {},
-    body: any = null
-  ): Promise<T> {
-    const defaultHeaders: Record<string, string> = {
-      "Content-Type": "application/json",
-      ...headers,
-    };
-
-    const options: RequestInit = {
-      method,
-      headers: defaultHeaders,
-    };
-
-    if (body) {
-      options.body = JSON.stringify(body);
-    }
-
-    const response = await fetch(`${this.baseUrl}${endpoint}`, options);
-
-    if (!response.ok) {
-      const responseJson = await response.json();
-      throw new Error(responseJson.message);
-    }
-    if (method === "DELETE") {
-      return response as T;
-    }
-    return await response.json();
+  constructor() {
+    this.httpClient = new HttpClient(this.baseUrl);
   }
 
   // Example functions
   public async getUserData(userId: number): Promise<any> {
-    return this.request<any>(`/users/${userId}`);
+    return this.httpClient.request<any>(`/users/${userId}`);
   }
 
   public async postData(endpoint: string, data: any): Promise<any> {
-    return this.request<any>(endpoint, "POST", {}, data);
+    return this.httpClient.request<any>(endpoint, "POST", {}, data);
   }
 
   //AUTHENTICATION
   public async login(data: any): Promise<AuthUser> {
-    return this.unauthenticatedRequest<AuthUser>(
+    return this.httpClient.unauthenticatedRequest<AuthUser>(
       apiUrls.auth.login,
       "POST",
       {},
@@ -128,24 +50,27 @@ class ApiClient {
     if (size) params.set("size", String(size));
     if (portalId) params.set("portalId", portalId);
 
-    return this.request<PaginatedResponse<User>>(
+    return this.httpClient.request<PaginatedResponse<User>>(
       `${apiUrls.users.get}?${params.toString()}`
     );
   }
 
   public async postUser(data: UserPayload): Promise<User> {
-    return this.request<User>(apiUrls.users.post, "POST", {}, data);
+    return this.httpClient.request<User>(apiUrls.users.post, "POST", {}, data);
   }
 
   public async deleteUser(data: { userId: string }): Promise<void> {
     const { userId } = data;
-    return this.request(`${apiUrls.users.delete}/${userId}`, "DELETE");
+    return this.httpClient.request(
+      `${apiUrls.users.delete}/${userId}`,
+      "DELETE"
+    );
   }
 
   public async toggleUserAccess(data: { userId: string }): Promise<void> {
     const { userId } = data;
     const url = apiUrls.users.toggleAccess(userId);
-    return this.request(url, "PUT");
+    return this.httpClient.request(url, "PUT");
   }
 
   public async changePassword(data: {
@@ -153,7 +78,7 @@ class ApiClient {
     newPassword: string;
   }): Promise<User> {
     const { newPassword, userId } = data;
-    return this.request<User>(
+    return this.httpClient.request<User>(
       `${apiUrls.users.changePassword}?newPassword=${newPassword}&userId=${userId}`,
       "PUT"
     );
@@ -161,11 +86,11 @@ class ApiClient {
 
   //EVENTS
   public async getEvents(): Promise<EventType[]> {
-    return this.request<EventType[]>(apiUrls.events.get);
+    return this.httpClient.request<EventType[]>(apiUrls.events.get);
   }
 
   public async deleteEventById(eventId: string): Promise<EventType[]> {
-    return this.request<EventType[]>(
+    return this.httpClient.request<EventType[]>(
       apiUrls.events.deleteEventById(eventId),
       "DELETE"
     );
@@ -174,7 +99,7 @@ class ApiClient {
   public async getSwoogoEvents(): Promise<
     { eventId: string; eventName: string }[]
   > {
-    return this.request<{ eventId: string; eventName: string }[]>(
+    return this.httpClient.request<{ eventId: string; eventName: string }[]>(
       apiUrls.events.getSwoogoEvents
     );
   }
@@ -183,22 +108,29 @@ class ApiClient {
     payload: any,
     method: "POST" | "PUT"
   ): Promise<EventType> {
-    return this.request(apiUrls.events.saveEvent, method, {}, payload);
+    return this.httpClient.request(
+      apiUrls.events.saveEvent,
+      method,
+      {},
+      payload
+    );
   }
 
   //PORTALS MANAGEMENT
   public async getEventPortals(eventId: string): Promise<Portal[]> {
-    return this.request<Portal[]>(
+    return this.httpClient.request<Portal[]>(
       apiUrls.portalsManagement.getEventPortals(eventId)
     );
   }
 
   public async getPortalTemplates(): Promise<Portal[]> {
-    return this.request<Portal[]>(apiUrls.portalsManagement.getPortalTemplates);
+    return this.httpClient.request<Portal[]>(
+      apiUrls.portalsManagement.getPortalTemplates
+    );
   }
 
   public async getPortalDetails(portalId: string): Promise<Portal> {
-    return this.request<Portal>(
+    return this.httpClient.request<Portal>(
       apiUrls.portalsManagement.getPortalDetails(portalId)
     );
   }
@@ -208,11 +140,14 @@ class ApiClient {
     templateId: string
   ): Promise<void> {
     const url = apiUrls.portalsManagement.addPortalToEvent(eventId);
-    return this.request<void>(`${url}?templateId=${templateId}`, "PUT");
+    return this.httpClient.request<void>(
+      `${url}?templateId=${templateId}`,
+      "PUT"
+    );
   }
 
   public async removePortalFromEvent(portalId: string): Promise<void> {
-    return this.request<void>(
+    return this.httpClient.request<void>(
       apiUrls.portalsManagement.removePortalFromEvent(portalId),
       "DELETE"
     );
@@ -225,7 +160,7 @@ class ApiClient {
   ): Promise<void> {
     const params = new URLSearchParams();
     params.set("scope", scope);
-    return this.request<void>(
+    return this.httpClient.request<void>(
       `${apiUrls.portalsManagement.updateCustomFields(
         portalId
       )}?${params.toString()}`,
@@ -246,18 +181,18 @@ class ApiClient {
   ): Promise<void> {
     const params = new URLSearchParams();
     params.set("purpose", purpose);
+    const url =
+      apiUrls.files.uploadAndCreateJob(portalId) + "?" + params.toString();
     const formData = new FormData();
     formData.append("file", file);
-    return this.request<void>(
-      apiUrls.files.uploadAndCreateJob(portalId) + "?" + params.toString(),
-      "POST",
-      {},
-      formData
-    );
+
+    return this.httpClient.multiPartRequest<void>(url, "POST", {}, formData);
   }
 
   public async getJobs(portalId: string): Promise<PaginatedResponse<Job>> {
-    return this.request<PaginatedResponse<Job>>(apiUrls.files.jobs(portalId));
+    return this.httpClient.request<PaginatedResponse<Job>>(
+      apiUrls.files.jobs(portalId)
+    );
   }
 }
 
