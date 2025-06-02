c8c1c8dff4bf61a156d8935c85cb115160683bba
Sikshya Maharjan <disengagedhero666@gmail.com>
2025-06-02 15:01:10 +0545

    chore: update the urls

--- src/consts/urls.ts
+++ src/consts/urls.ts
@@ -1,7 +1,7 @@
 export const PRESENTATION_TEMPLATE_FILE_NAME = "presentation_template.xlsx";
 export const PRESENTATION_TEMPLATE_URL =
-  "https://airdrive.eventsair.com/eventsairwesteuprod/production-cirse-public/cee82f5134b2497199b528ea2a747557";
+  "https://airdrive.eventsair.com/eventsairwesteuprod/production-cirse-public/633f1f434a7c4246952c1ea7d48f3455";
 
 export const SESSION_TEMPLATE_FILE_NAME = "session_template.xlsx";
 export const SESSION_TEMPLATE_URL =
-  "https://airdrive.eventsair.com/eventsairwesteuprod/production-cirse-public/a427db1f66184e609688dbd9317a1762";
+  "https://airdrive.eventsair.com/eventsairwesteuprod/production-cirse-public/e8d546505daa41c780627550fa16c550";
