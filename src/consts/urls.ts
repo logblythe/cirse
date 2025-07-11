bd234c61887a895ace11ce1ed78de2149aee02a5
Sikshya Maharjan <disengagedhero666@gmail.com>
2025-07-11 12:20:33 +0545

    chore: update template url

--- src/consts/urls.ts
+++ src/consts/urls.ts
@@ -4,4 +4,4 @@ export const PRESENTATION_TEMPLATE_URL =
 
 export const SESSION_TEMPLATE_FILE_NAME = "session_template.xlsx";
 export const SESSION_TEMPLATE_URL =
-  "https://airdrive.eventsair.com/eventsairwesteuprod/production-cirse-public/e8d546505daa41c780627550fa16c550";
+  "https://airdrive.eventsair.com/eventsairwesteuprod/production-cirse-public/c42f612b164a42bebeac09bebe1642d7";
